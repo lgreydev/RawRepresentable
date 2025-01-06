dfec97639796ce4140b9fe82b8298e5f10da1a0b
Serhii Lukashchuk <s.lukaschuk@yahoo.com>
2025-01-05 20:36:34 -0500

    Initial Commit

---
+++ RawRepresentable/RawRepresentableApp.swift
@@ -0,0 +1,17 @@
+//
+//  RawRepresentableApp.swift
+//  RawRepresentable
+//
+//  Created by Serhii Lukashchuk on 2025-01-05.
+//
+
+import SwiftUI
+
+@main
+struct RawRepresentableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
