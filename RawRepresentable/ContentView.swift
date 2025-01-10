abdefd921a4997d787a63ed492765d44528086c4
Serhii Lukashchuk <s.lukaschuk@yahoo.com>
2025-01-09 19:54:54 -0500

    add text field and toggle

--- RawRepresentable/ContentView.swift
+++ RawRepresentable/ContentView.swift
@@ -9,10 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
+        @AppStorage("name") var name: String = ""
+        @AppStorage("allowNotification") var allowNotification: Bool = false
         @AppStorage("skillLevel") var skillLevel: SkillLevel = .beginner
         
         NavigationStack {
             Form {
+                
+                TextField("Name", text: $name)
+                    .textFieldStyle(.roundedBorder)
+                
+                Toggle("Allow Notification", isOn: $allowNotification)
+                
                 Picker("Skill Level", selection: $skillLevel) {
                     ForEach(SkillLevel.allCases) { skillLevel in
                         Text(skillLevel.rawValue.capitalized)
